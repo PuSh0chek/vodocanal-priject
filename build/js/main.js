aaf854368f9f2c0e2ca9334bd2a45df3a869cdfc
Ганин Александр Владимирович <87208475+PuSh0chek@users.noreply.github.com>
2022-11-16 15:00:49 +0300

    7.5.3.2.021 popup + затемнение

--- build/js/main.js
+++ build/js/main.js
@@ -7,6 +7,9 @@ const outfits = document.querySelector('.outfits');
 const menuButtonChecking = document.querySelector('.navigation__button-ckecking-graf');
 const menuButtonAttire = document.querySelector('.navigation__button-attire');
 const menuButtonTrial = document.querySelector('.navigation__button-trial');
+const checkingPopup = document.querySelector('.checking__popup');
+const checkingPopupButtonOut = document.querySelector('.checking__popup-button-out');
+const ckeckingButtonAdd = document.querySelector('.checking__button-add');
 // const indocatorContent = document.querySelector('.trail__indicator');
 
 //// уменьшение шрифта при заполнености контейнера //////////////
@@ -37,12 +40,24 @@ const sortElements = item => {
 for (let i = 0; i < filterItems.length; i++) {
   sortElements(filterItems[i]);
 }
-const checkingPopup = document.querySelector('.checking__popup');
-const checkingPopupButtonOut = document.querySelector('.checking__popup-button-out');
-const ckeckingButtonAdd = document.querySelector('.checking__button-add');
+
+// Всплытие popup по кнопке добавить
 ckeckingButtonAdd.addEventListener('click', () => {
   checkingPopup.classList.remove('checking__popup--display-none');
 });
 checkingPopupButtonOut.addEventListener('click', () => {
   checkingPopup.classList.add('checking__popup--display-none');
+});
+const popUpDelete = document.querySelector('.checking__popup-delete');
+const buttonDelete = document.querySelector('.checking__button-delete');
+const buttonDeleteOut = document.querySelector('.checking__popup-delete-button-no');
+
+// Всплытие popup по кнопке удалить
+buttonDelete.addEventListener('click', () => {
+  popUpDelete.classList.remove('checking__popup-delete--display-none');
+});
+
+// Исчезновение popup по кнопке НЕТ
+buttonDeleteOut.addEventListener('click', () => {
+  popUpDelete.classList.add('checking__popup-delete--display-none');
 });
--- source/js/main.js
+++ source/js/main.js
@@ -5,6 +5,9 @@ const outfits = document.querySelector('.outfits');
 const menuButtonChecking = document.querySelector('.navigation__button-ckecking-graf');
 const menuButtonAttire = document.querySelector('.navigation__button-attire');
 const menuButtonTrial = document.querySelector('.navigation__button-trial');
+const checkingPopup = document.querySelector('.checking__popup');
+const checkingPopupButtonOut = document.querySelector('.checking__popup-button-out');
+const ckeckingButtonAdd = document.querySelector('.checking__button-add');
 // const indocatorContent = document.querySelector('.trail__indicator');
 
 //// уменьшение шрифта при заполнености контейнера //////////////
@@ -79,10 +82,7 @@ const inspectionsDate = [
   },
 ];
 
-const checkingPopup = document.querySelector('.checking__popup');
-const checkingPopupButtonOut = document.querySelector('.checking__popup-button-out');
-const ckeckingButtonAdd = document.querySelector('.checking__button-add');
-
+// Всплытие popup по кнопке добавить
 ckeckingButtonAdd.addEventListener('click', () => {
   checkingPopup.classList.remove('checking__popup--display-none');
 });
@@ -91,3 +91,17 @@ checkingPopupButtonOut.addEventListener('click', () => {
   checkingPopup.classList.add('checking__popup--display-none');
 });
 
+
+const popUpDelete = document.querySelector('.checking__popup-delete');
+const buttonDelete = document.querySelector('.checking__button-delete');
+const buttonDeleteOut = document.querySelector('.checking__popup-delete-button-no');
+
+// Всплытие popup по кнопке удалить
+buttonDelete.addEventListener('click', () => {
+  popUpDelete.classList.remove('checking__popup-delete--display-none');
+});
+
+// Исчезновение popup по кнопке НЕТ
+buttonDeleteOut.addEventListener('click', () => {
+  popUpDelete.classList.add('checking__popup-delete--display-none');
+});
